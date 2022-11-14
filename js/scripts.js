3869f096b272a9a69d8a5f801ccf95e1ed3c4076
riskerGoldClass <roakfwk30@naver.com>
2022-11-15 00:09:39 +0900

    add : api 연동 및 차트 작성

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,46 @@
     * Copyright 2013-2022 Start Bootstrap
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
-    // 
+    //
 // Scripts
 //
 
 /**
- * Power State
+ * 전원 동작 출력 부분
+ * Power state
  */
-//System off
-let state = 1;
-if (state === 0) {
+let state = {
+    value : 2
+}; //임시 선언
+
+//System Stop
+if (state.value === 0) {
     $(function (){
         $(".circle").css({
             "border": "17px #DC3545 solid"
         })
     })
 }
-//System on
-else if (state === 1) {
+//System Start
+else if (state.value === 1) {
     $(function (){
         $(".circle").css({
             "border": "17px #198754 solid"
         })
     })
 }
+//System Pause
+else{
+    $(function (){
+        $(".circle").css({
+            "border": "17px #F27B25 solid"
+        })
+    })
+}
 
 /**
- * Time
+ * 현재 시간 출력 부분
+ * Present Time
  */
 function clock(){
     let date = new Date();
@@ -40,6 +53,15 @@ function clock(){
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
+    // 오전 or 오후 저장 변수
+    let amOrPm = "AM";
+
+    // 12시 이상일 시
+    if (hours >= 12) {
+        amOrPm = "PM";
+        hours -= 12;
+    }
+
     let hours_str = hours < 10 ? "0"+hours : hours;
     let minutes_str = minutes < 10 ? "0"+minutes : minutes;
     let seconds_str = seconds < 10 ? "0"+seconds : seconds;
@@ -48,24 +70,175 @@ function clock(){
         clockDate = "0" + clockDate;
     }
 
-    $(".bn_date").html(nowYear + "-" + (month+1) + "-" + clockDate + " " + hours_str + ":"+minutes_str + ":" + seconds_str + "(24h)");
+    $(".bn_date").html(nowYear + "-" + (month+1) + "-" + clockDate + " " + //날짜
+        amOrPm + hours_str + ":"+minutes_str + ":" + seconds_str);         //시간
 
 }
+
+/**
+ * API Call / Parsing
+ */
+const url = "http://121.178.2.4:9000/api?api_key=2a618e17de2f851d74216ddef0256bc1";
+function apiCall() {
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let rack2 = data.rack2[0]; //배터리실 온ㆍ습도
+            let rack3 = data.rack3[0]; //실내 온ㆍ습도 1
+            let rack4 = data.rack4[0]; //실내 온ㆍ습도 2
+            let rack5 = data.rack5[0]; //실내 온ㆍ습도 3
+
+            $("#batteryRoom-tem").html("온도 : " + rack2['sd1'] + " (℃)");
+            $("#batteryRoom-hum").html("습도 : " + rack2['sd2'] + " (%)");
+            drawChart(rack2['sd1'], rack2['sd2'], "batteryRoom-chart");
+
+            $("#indoor1-tem").html("온도 : " + rack3['sd1'] + " (℃)");
+            $("#indoor1-hum").html("습도 : " + rack3['sd2'] + " (%)");
+            drawChart(rack3['sd1'], rack3['sd2'], "indoor1-chart");
+
+            $("#indoor2-tem").html("온도 : " + rack4['sd1'] + " (℃)");
+            $("#indoor2-hum").html("습도 : " + rack4['sd2'] + " (%)");
+            drawChart(rack4['sd1'], rack4['sd2'], "indoor2-chart");
+
+            $("#indoor3-tem").html("온도 : " + rack5['sd1'] + " (℃)");
+            $("#indoor3-hum").html("습도 : " + rack5['sd2'] + " (%)");
+            drawChart(rack5['sd1'], rack5['sd2'], "indoor3-chart");
+
+        });
+}
+
+/**
+ * 임시
+ * 난수 생성
+ * 배터리 온도, 금일 충방전량, 배터리 용량
+ */
+function randomNumberMaker(){
+    const batteryTem = Math.floor(Math.random() * 50);
+
+    const todayCharge = Math.floor(Math.random() * 80) + 30;
+    const todayDischarge = Math.floor(Math.random() * 50) + 30;
+
+    const batteryVolume = Math.floor(Math.random() * 50) + 50;
+
+    const batteryVolt = Math.floor(Math.random() * 20) + 40;
+    const batteryAmpere = Math.floor(Math.random() * 20) + 40;
+
+    $("#battery-tem").html("온도 : " + batteryTem + "(℃)");
+    drawChart2(batteryTem, "batteryTem-chart");
+
+    $("#today-charge").html("충전량 : " + todayCharge + " (kWh)");
+    $("#today-discharge").html("방전량 : " + todayDischarge + " (kWh)");
+    drawChart(todayCharge, todayDischarge, "today-charge-discharge-chart");
+
+    $("#battery-volume").html("용량 : " + batteryVolume + "% (SoC)");
+    drawChart2(batteryTem, "batteryVolume-chart");
+
+    $("#battery-volt").html("전압 : " + batteryVolt + " (V)");
+    $("#battery-ampere").html("전류 : " + batteryAmpere + " (A)");
+    drawChart(batteryVolt, batteryAmpere, "battery-volt-ampere-chart");
+
+}
+
 function init() {
     clock();
+    apiCall()
+    randomNumberMaker()
 
-    setInterval(clock, 1000);
+    setInterval(clock, 1000);    //현재 시간 1초 루프
+    setInterval(apiCall, 10000); //API 10초 루프
+    setInterval(randomNumberMaker, 10000);
 }
 $(document).ready(function(){
     init();
 });
 
+/**
+ * Google Chart
+ */
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
 
+function drawChart(tem, hum, tagName) {
+    let data;
+    if (tagName === "today-charge-discharge-chart"){
+        data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['Charging', Number(tem)],
+            ['Discharging', Number(hum)],
+        ]);
+    }else if(tagName === "battery-volt-ampere-chart"){
+        data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['Volt', Number(tem)],
+            ['Ampere', Number(hum)],
+        ]);
+    }else{
+        data = google.visualization.arrayToDataTable([
+            ['Label', 'Value'],
+            ['Temperature', Number(tem)],
+            ['Humidity', Number(hum)],
+        ]);
+    }
 
+    let options = {
+        width: "100%", height: "100%",
+        yellowFrom:75, yellowTo: 90,
+        redFrom: 90, redTo: 100,
+        minorTicks: 5,
+        legend: "test"
+    };
 
+    let chart = new google.visualization.Gauge(document.getElementById(tagName));
 
+    chart.draw(data, options);
 
+    //차트 크기 반응형 핸들러 -start
+    function resizeHandler () {
+        chart.draw(data, options);
+    }
+    if (window.addEventListener) {
+        window.addEventListener('resize', resizeHandler, false);
+    }
+    else if (window.attachEvent) {
+        window.attachEvent('onresize', resizeHandler);
+    }
+    //차트 크기 반응형 핸들러 -end
+}
 
+//인수 2개 짜리 차트
+function drawChart2(tem, tagName) {
+
+    let data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Temperature', Number(tem)]
+    ]);
+
+    let options = {
+        width: "100%", height: "100%",
+        yellowFrom:75, yellowTo: 90,
+        redFrom: 90, redTo: 100,
+        minorTicks: 5,
+        legend: "test"
+    };
+
+    let chart = new google.visualization.Gauge(document.getElementById(tagName));
+
+    chart.draw(data, options);
+
+    //차트 크기 반응형 핸들러 -start
+    function resizeHandler () {
+        chart.draw(data, options);
+    }
+    if (window.addEventListener) {
+        window.addEventListener('resize', resizeHandler, false);
+    }
+    else if (window.attachEvent) {
+        window.attachEvent('onresize', resizeHandler);
+    }
+    //차트 크기 반응형 핸들러 -end
+}
 
 
 
